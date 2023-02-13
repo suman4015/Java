cf83679e2daa6a5c7ed78095c99192139018d826
suman4015 <60983585+suman4015@users.noreply.github.com>
2023-02-13 17:50:26 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello
     {
-        public static void Main(String[] args)
+        public static void main(String[] args)
         {
             System.out.println("Hello World from git!");
         }
